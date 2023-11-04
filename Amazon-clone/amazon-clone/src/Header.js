54e52bd0fd408bb58dbf06e2e06fcd869af35f6d
akpradeep62 <akpradeep62@gmail.com>
2023-11-04 19:50:44 +0000

    Amazon nav bar design complted

--- Amazon-clone/amazon-clone/src/Header.js
+++ Amazon-clone/amazon-clone/src/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './Header.css'
-import SearchIcon from '@mui/icons-material/Search';
+import { Search } from 'lucide-react';
+import { ShoppingCart } from 'lucide-react';
 
 function Header() {
   return (
     <div className='header'>
       <img className='header_logo' src = "https://pngimg.com/uploads/amazon/amazon_PNG1.png"/>
-    
     <div className='header_search'>
         <input className='header_searchinput'/>
-        <SearchIcon />
+        <Search className='Header_searchIcon' />
     </div>
 
     <div className='Header_nav'>
@@ -18,13 +18,17 @@ function Header() {
             <span className='header_option_linetwo'>Sign in</span>
         </div>
         <div className='header_option'>
-        <span className='header_option_lineone'>Rerurns</span>
+        <span className='header_option_lineone'>Returns</span>
         <span className='header_option_linetwo'>&Orders</span>
 
         </div>
         <div className='header_option'>
         <span className='header_option_lineone'>Your</span>
         <span className='header_option_linetwo'>Prime</span>
+        </div>
+        <div className='header_optionbasket'>
+        <ShoppingCart />
+        <span className='header_option_linetwo'>0</span>
         </div>
     
     </div>
