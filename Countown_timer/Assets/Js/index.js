240240dac31645f8b947408e80ec2a6d64f730df
akpradeep62 <akpradeep62@gmail.com>
2024-10-26 07:34:38 -0400

    minor changes

--- Countown_timer/Assets/Js/index.js
+++ Countown_timer/Assets/Js/index.js
@@ -1,16 +1,7 @@
 const days = document.querySelector('#days');
-
 const hours  = document.querySelector('#hours');
-
 const minitues  = document.querySelector('#minitues');
-
 const seconds = document.querySelector('#seconds');
-
-
-
-
-
-
 
 function UpdateTime(){
     const current_year = new Date().getFullYear();
@@ -18,17 +9,16 @@ function UpdateTime(){
     const currentdate = new Date();
     const diff = newYear -currentdate;
     const day = Math.floor(diff/1000/60/60/24);
-    const hour = Math.floor((diff/1000/60/60)%24)
-    const min = Math.floor((diff/1000/60)%60)
-    const s = Math.floor((diff/1000)%60)
-
-days.innerHTML=day
-hours.innerHTML=hour
-minitues.innerHTML=min
-seconds.innerHTML=s
+    const hour = Math.floor((diff/1000/60/60)%24);
+    const min = Math.floor((diff/1000/60)%60);
+    const s = Math.floor((diff/1000)%60);
+    days.innerHTML=day;
+    hours.innerHTML=hour;
+    minitues.innerHTML=min;
+    seconds.innerHTML=s;
     
 }
-setInterval(UpdateTime,1000)
+setInterval(UpdateTime,1000);
 
 
 
